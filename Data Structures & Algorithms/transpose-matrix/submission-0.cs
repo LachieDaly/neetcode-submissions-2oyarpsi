fac4e51bf4d43003f6cb10341eb7922ec9fc0217
Lachlan Daly <63344612+LachieDaly@users.noreply.github.com>
2026-04-11 13:06:16 +1000

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[][] Transpose(int[][] matrix) {
+        int[][] transposedMatrix = new int[matrix[0].Length][];
+        for (int i = 0; i < matrix[0].Length; i++)
+        {
+            transposedMatrix[i] = new int[matrix.Length];
+            for (int j = 0; j < matrix.Length; j++) 
+            {
+                transposedMatrix[i][j] = matrix[j][i];
+            }
+        }
+        return transposedMatrix;
+    }
+}
